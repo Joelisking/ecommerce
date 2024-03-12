9a6e480b22b8413cfc5432468f550e61e039f8c3
JoelisKing <93987931+Joelisking@users.noreply.github.com>
2024-03-12 16:08:19 +0000

    logo & footer fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import Head from 'next/head'
+import React from 'react';
+import Head from 'next/head';
 
-import Navbar from './Navbar'
-import Footer from './Footer'
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
-	return (
-		<div className="layout">
-			<Head>
-				<title>JSM Headphones</title>
-			</Head>
-			<header>
-				<Navbar />
-			</header>
-			<main className="main-container">{children}</main>
-			<footer>
-				<Footer />
-			</footer>
-		</div>
-	)
-}
+  return (
+    <div className="layout">
+      <Head>
+        <title>JAK Headphones</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
